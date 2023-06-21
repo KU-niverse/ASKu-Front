f756f8c4a3498506076701b8e81f8eaeeaa6913e
noviceo <2000robert@naver.com>
2023-06-21 19:22:21 +0900

    header 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import { Link } from 'react-router-dom';
+import styles from './Footer.module.css';
 
 function Footer() {
+    return(
+        <div className='container'>
 
+        </div>
+    )
 }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,24 @@ import styles from './Header.module.css';
 function Header() {
     return (
         <div className={`${styles.container}`}>
+            <div className={`${styles.headerContainer}`}>
+                <div>
+                <Link to=''>
+                    <button>최근 변경</button>
+                </Link>
+                <Link to=''>
+                    <button>토론</button>
+                </Link>
+                </div>
+                <div>
+                <Link to=''>
+                    <button>회원가입</button>
+                </Link>
+                <Link to=''>
+                    <button>로그인</button>
+                </Link>
+                </div>
+            </div>
         </div>
     )
 }
