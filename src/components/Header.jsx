559fed82f964556767d2cc26343193d5648373c2
noviceo <2000robert@naver.com>
2023-06-22 11:40:36 +0900

    align-items 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import logo from '../img/logo.png';
 
 function Header() {
     return (
         <div className={`${styles.container}`}>
             <div className={`${styles.headerContainer}`}>
                 <div>
+                    <img src={logo} alt='logo' className={`${styles.logo}`} />
+                </div>
+                <div className={`${styles.navButtons}`}>
                     <Link to=''>
                         <button>최근 변경</button>
                     </Link>
@@ -13,10 +17,10 @@ function Header() {
                         <button>토론</button>
                     </Link>
                 </div>
-                <div>
+                <div className={`${styles.searchInput}`}>
                     <input placeholder='검색어를 입력하세요.'/>
                 </div>
-                <div>
+                <div className={`${styles.navButtons}`}>
                     <Link to=''>
                         <button>회원가입</button>
                     </Link>
