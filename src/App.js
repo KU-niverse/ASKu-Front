8bdbfcd056ca84948e9ec6fd5e121f48508bf088
mkngkm <mikang1031@gmail.com>
2023-06-23 09:30:45 +0900

    feat: editor 설정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
+import WikiEdit from './pages/WikiEdit';
 
 function App() {
     return ( 
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/wikiedit" element={<WikiEdit />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/WikiEdit.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react';
+import Editor from '../components/EditorComponent'
+import styles from './WikiEdit.module.css';
+import Header from '../components/Header';
+
+
+const WikiEdit = () => {
+    const [desc, setDesc] = useState('');
+    function onEditorChange(value) {
+        setDesc(value)
+    }
+    
+    return (
+        <div>
+            <Header />
+            <div className={`${styles.edit}`}>
+                <div>
+                    <h4>문서 제목</h4>
+                    <input type='text'/>
+                </div>
+                <div>
+                    <h4>문서 내용</h4>
+                    <div>
+                    <Editor value={desc} onChange={onEditorChange} />
+                    </div>
+                    <h4>히스토리 요약</h4>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <span>정책에 맞게 작성하였음을 확인합니다.</span>
+                    <button>생성하기</button>
+                </div>
+            </div>
+            
+            
+        </div>
+        
+    )
+
+}
+
+export default WikiEdit;
+
