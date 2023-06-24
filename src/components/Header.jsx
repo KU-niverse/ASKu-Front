515ba6f14c035daa7bbccee0428195c61ff056ed
noviceo <2000robert@naver.com>
2023-06-24 15:36:47 +0900

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,34 +2,40 @@ import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../img/logo.png';
 import searchIcon from '../img/search_icon.png';
+import searchIconGray from '../img/search_icon_gray.png';
+import hamburger from '../img/hamburger.png';
 
 function Header() {
     return (
-        <div className={`${styles.container}`}>
-            <div className={`${styles.headerContainer}`}>
-                <div className={`${styles.logoContainer}`}>
-                    <img src={logo} alt='logo' className={`${styles.logo}`} />
+        <div className={styles.container}>
+            <div className={styles.headerContainer}>
+                <div className={styles.logoContainer}>
+                    <img src={logo} alt='logo' className={styles.logo} />
                 </div>
-                <div className={`${styles.flexContainer}`}>
-                    <div className={`${styles.navContainer}`}>
+                <div className={styles.flexContainer}>
+                    <div className={styles.navContainer}>
                         <Link to=''>
-                            <button className={`${styles.headerButton}`}>최근 변경</button>
+                            <button className={styles.headerButton}>최근 변경</button>
                         </Link>
                         <Link to=''>
-                            <button className={`${styles.headerButton}`}>토론</button>
+                            <button className={styles.headerButton}>토론</button>
                         </Link>
                     </div>
-                    <div className={`${styles.inputContainer}`}>
-                        <input className={`${styles.headerInput}`} placeholder=' 검색어를 입력하세요.'/>
-                        <img src={searchIcon} alt='icon' className={`${styles.searchIcon}`} />
+                    <div className={styles.inputContainer}>
+                        <input className={styles.headerInput} placeholder=' 검색어를 입력하세요.' />
+                        <img src={searchIcon} alt='icon' className={styles.searchIcon} />
                     </div>
-                    <div className={`${styles.navContainer}`}>
+                    <div className={styles.navContainer}>
                         <Link to=''>
-                            <button className={`${styles.headerButton}`}>회원가입</button>
+                            <button className={styles.headerButton}>회원가입</button>
                         </Link>
                         <Link to=''>
-                            <button className={`${styles.headerButton}`}>로그인</button>
+                            <button className={styles.headerButton}>로그인</button>
                         </Link>
+                    </div>
+                    <div className={styles.buttonWrap}>
+                        <img src={searchIconGray} alt='search_icon_gray' className={styles.mobileButton} />
+                        <img src={hamburger} alt='hamburger' className={styles.mobileButton} />
                     </div>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Link } from 'react-router-dom';
+import logo from '../img/logo.png';
+import styles from './Home.module.css';
 
 function Home() {
     return (
         <div className="pageWrap">
             <Header />
+            <div className={`${styles.homeWrap}`}>
+
+            </div>
             <Footer />
         </div>
     )
