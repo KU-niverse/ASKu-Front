4b94e73d6f521d26f78b17499bd415d2be3bfc3d
mkngkm <mikang1031@gmail.com>
2023-06-26 15:02:05 +0900

    style: css 수정

--- src/pages/Wikiviewer.jsx
+++ src/pages/Wikiviewer.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom/dist";
 import React, {useRef, useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom/dist';
 import styles from './Wikiviewer.module.css';
-import { BsBookmarkFill, BsBookmark } from "react-icons/bs";
-import { IoIosPeople } from "react-icons/io";
-import { GrDocumentText } from "react-icons/gr";
+import bookmark from '../img/bookmark.png';
+import bookmarkFill from '../img/bookmarkFill.png';
+import debate from '../img/debate.png'
+import his from '../img/his.png'
 import WikiBox from '../components/WikiBox';
 import Switch from '../components/Switch';
 
@@ -13,6 +14,7 @@ function WikiViewer() {
     const myDivRef = useRef([]);
     const nav = useNavigate();
     const [isToggled, setIsToggled] = useState(false);
+    const [isBookmark, setIsBookmark] = useState(false);
 
     const data = [
         {
@@ -46,22 +48,28 @@ function WikiViewer() {
         
     }
 
+    function handleClickBookmark() {
+        setIsBookmark(!isBookmark);
+    }
+
     return (
         <div className={styles.container}>
             <Header />
             <div className={styles.wikiviewer}>
                <div className={styles.wikititle}>
-                  <h1>입실렌티<BsBookmark/><BsBookmarkFill/></h1>
-                  <div className={styles.wikititle_btn}>
-                    <button><IoIosPeople/>토론하기</button>
-                    <button><GrDocumentText/>히스토리</button>
+                  <h1>입실렌티<img src={bookmark} className={isBookmark ? `${styles.hidden}` : `${styles.bookmarkImg}`} onClick={handleClickBookmark}/>
+                    <img src={bookmarkFill} className={isBookmark ? `${styles.bookmarkImg}`: `${styles.hidden}`} onClick={handleClickBookmark}/>
+                  </h1>
+                  <div className={styles.wikititleBtn}>
+                    <button><img src={debate}/>&nbsp;토론하기</button>
+                    <button><img src={his}/>&nbsp;히스토리</button>
                   </div>
                </div>
                <div className={styles.wikiBoxLists}>
                 <div className={styles.wikilist}>
                     <div className={styles.wikilistTitle}>
                         <h2>목차</h2>
-                        <button>전체편집</button>
+                        <button>전체 편집</button>
                     </div>
                     <div>
                         {data.map((item) => {
@@ -75,7 +83,11 @@ function WikiViewer() {
                     
                 </div>
                 <div className={styles.wikiask}>
-                    <Switch  isToggled={isToggled} onToggle={() => setIsToggled(!isToggled)}/>
+                    <div className={styles.wikiaskTitle}>
+                        <h2>질문</h2>
+                        <Switch  isToggled={isToggled} onToggle={() => setIsToggled(!isToggled)}/>
+                    </div>
+                    
                 </div>
                 <div className={styles.wikiwrite}></div>
                </div>
