f6644acc67887f1c756c2963d4ba22ae18f5af31
dhyun22 <sandy2873@naver.com>
2023-09-04 15:22:22 +0900

    api 경로 수정

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -40,7 +40,7 @@ function Chatbot () {
 
     const checkLoginStatus = async () => {
         try {
-            const res = await axios.get("http://localhost:8080/user/auth/issignedin", {
+            const res = await axios.get("https://asku.wiki/api/user/auth/issignedin", {
                 withCredentials: true
             });
             if (res.status === 201 && res.data.success === true) {
@@ -56,7 +56,7 @@ function Chatbot () {
     
     const getUserInfo = async () => {
         try {
-            const res = await axios.get("http://localhost:8080/user/mypage/info", {
+            const res = await axios.get("https://asku.wiki/api/user/mypage/info", {
                 withCredentials: true
             });
             if (res.status === 201 && res.data.success === true) {
