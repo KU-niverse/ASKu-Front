6cc20e48c681cc018d7672f353bca97967687233
youngsupchoi <sup1214@naver.com>
2023-12-07 22:42:31 +0900

    Fix #178: 닉네임 수정 버튼 비활성화

--- src/components/Mypage/MyProfile.jsx
+++ src/components/Mypage/MyProfile.jsx
@@ -5,19 +5,19 @@ import BadgeModal from "../BadgeModal";
 import NickEditModal from "./NickEditModal"
 import { useNavigate } from "react-router-dom";
 
-function MyProfile({ nick, point, badge, percent, badgeimg}){
+function MyProfile({ nick, point, badge, percent, badgeimg }) {
   // const [isBadgeModalVisible, setBadgeModalVisible] = useState(false);
   // const closeBadgeModal = () => {
   //     setBadgeModalVisible(false);
   // };
   const nav = useNavigate();
-  const linktoBadge = ()=>{
+  const linktoBadge = () => {
     nav(`/mypage/mybadge`)
-  } 
+  }
 
   const [isEditModalVisible, setEditModalVisible] = useState(false);
   const closeEditModal = () => {
-      setEditModalVisible(false);
+    setEditModalVisible(false);
   };
 
   const [loggedIn, setLoggedIn] = useState(false);
@@ -26,28 +26,28 @@ function MyProfile({ nick, point, badge, percent, badgeimg}){
     setEditModalVisible(true);
   }
 
-  {isEditModalVisible && <NickEditModal isOpen={isEditModalVisible} onClose={() => setEditModalVisible(false)} />}
+  { isEditModalVisible && <NickEditModal isOpen={isEditModalVisible} onClose={() => setEditModalVisible(false)} /> }
 
 
 
-  return(
+  return (
     <div>
       <div className={styles.profileimg}>
-        <img className={styles.profileimg_content} onClick={linktoBadge} src={badgeimg} alt='haho'/>
+        <img className={styles.profileimg_content} onClick={linktoBadge} src={badgeimg} alt='haho' />
       </div>
       <div className={styles.profilerow}>
         <div className={styles.rownick}>
-            <span className={styles.rowtitle}>닉네임</span>
-            <div className={styles.text}>
-              <span className={styles.point}>{nick}</span>
-              <span className={styles.editbtn} onClick={handleNickEdit}>수정</span>
-              {isEditModalVisible && <NickEditModal isOpen={isEditModalVisible} onClose={() => setEditModalVisible(false)} />}
-            </div>
+          <span className={styles.rowtitle}>닉네임</span>
+          <div className={styles.text}>
+            <span className={styles.point}>{nick}</span>
+            <span className={styles.editbtn} onClick={handleNickEdit}>수정</span>
+            {isEditModalVisible && <NickEditModal isOpen={isEditModalVisible} onClose={() => setEditModalVisible(false)} />}
+          </div>
         </div>
         <div className={styles.rowbadge}>
           <span className={styles.rowtitle}>대표 뱃지</span>
           <span className={styles.text} onClick={linktoBadge}>{badge}
-          {/* <p className={`${styles.badgeedit}`}>뱃지 수정은 PC에서만 가능합니다.</p> */}
+            {/* <p className={`${styles.badgeedit}`}>뱃지 수정은 PC에서만 가능합니다.</p> */}
 
           </span>
           {/* <span className={styles.edit} onClick={setBadgeModalVisible(true)}>수정</span> */}
@@ -56,7 +56,7 @@ function MyProfile({ nick, point, badge, percent, badgeimg}){
         <div className={styles.rowpoint}>
           <span className={styles.rowtitle}>기여도</span>
           <div className={styles.text}>
-            <span className={styles.point}>{point}p</span> 
+            <span className={styles.point}>{point}p</span>
             <span className={styles.rank}> &nbsp;&nbsp;&nbsp;상위 {percent}%</span>
           </div>
         </div>
