604b00493a94deea746bc91029c87e75df50e9fe
MJ <alswns8495@naver.com>
2024-09-04 14:36:45 +0900

    feat : 메인페이지 '모든 문서' -> '최근 변경' 으로 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -284,7 +284,7 @@ function Header({ userInfo, setUserInfo }: any) {
                 }}
               >
                 <img src={all_document} alt={'모든 문서'} className={styles.icon} />
-                {ismainpage || buttonTextVisible ? '모든 문서' : ''}
+                {ismainpage || buttonTextVisible ? '최근 변경' : ''}
               </button>
             </Link>
             <Link
